cde9c8be6b3befdc336e630892dcdc392fa35654
nishantp15 <prajapati.nishant@outlook.com>
2023-06-21 20:40:41 +0530

    assignment

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import AuthProvider from "./components/AuthProvider/AuthProvider";
-import Navbar from './components/navbar/Navbar';
+// import AuthProvider from "./components/AuthProvider/AuthProvider";
+// import Navbar from './components/navbar/Navbar';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -13,10 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <AuthProvider>
-          <Navbar />
+   
+       
           {children}
-        </AuthProvider>
+       
         </body>
     </html>
   )
