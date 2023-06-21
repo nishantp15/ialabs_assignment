5f54d16038362d5a8ece73d96922fea2b4bbc08b
nishantp15 <prajapati.nishant@outlook.com>
2023-06-21 20:36:37 +0530

    assignment

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,21 +1,9 @@
-"use client";
-import { useRouter } from "next/navigation";
-import React, { useState } from "react";
-import { AppContext } from "../components/AuthProvider/AuthProvider";
-import { useContext } from "react";
-import styles from './Dashboard.module.css'
-const Dashboard = () => {
-  const { authState, logoutUser } = useContext(AppContext);
- 
-  let router = useRouter();
-  if (!authState.token) {
-    router?.push("/dashboard/login");
-  }
-  console.log(authState.token);
-  return <div className={styles.dashboard}>
-    <h1>Welcome</h1>
-    <p>No data available</p>
-  </div>
-};
+import React from 'react'
 
-export default Dashboard;
+const Dashboard = () => {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
