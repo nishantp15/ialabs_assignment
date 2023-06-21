dfd79960be3d27eb0058895121c21c2caf87b43d
nishantp15 <prajapati.nishant@outlook.com>
2023-06-21 21:27:56 +0530

    assignment

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,24 +1,20 @@
 "use client";
-
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 import { AppContext } from "../components/AuthProvider/AuthProvider";
 import { useContext } from "react";
 import styles from './Dashboard.module.css'
-import { useRouter } from "next/navigation";
-import Link from "next/link";
 const Dashboard = () => {
   const { authState, logoutUser } = useContext(AppContext);
  
   let router = useRouter();
   if (!authState.token) {
-    // router.push("/dashboard/login");
+    router?.push("/dashboard/login");
   }
-
+  console.log(authState.token);
   return <div className={styles.dashboard}>
     <h1>Welcome</h1>
-    {/* <p>No data available</p> */}
-    {/* <Link href={'dashboard/login'}>logoin</Link> */}
-    
+    <p>No data available</p>
   </div>
 };
 
